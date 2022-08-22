8d24c11769ac0e724be4ea3649a43e35416b8cec
Michelle <vantablanta@gmail.com>
2022-08-22 10:28:33 +0300

    updating state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      data: "Michelle Njeri"
+    }
+  }
+  updateUser(){
+    this.setState({data: "Morris Nuthumbi"})
+  }
+  render (){
+    return (
+      <div className="App">
+        <h5>Current User: {this.state.data}</h5>
+        <button onClick={()=>this.updateUser()}>Update User</button>
+      </div>
+    );
+  }
 }
 
 export default App;
